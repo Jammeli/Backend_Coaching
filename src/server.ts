0bee058db15e5d0a9e9c940fe3b53d244f48b6ed
Marwen - JAMMELI <marwen.jammeli@gmail.com>
2025-04-23 23:56:29 +0100

    Create server.ts

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cvParserRoute from "../routes/cvParserRoute";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use("/api", cvParserRoute);
+
+app.listen(PORT, () => {
+  console.log(`Backend en écoute sur http://localhost:${PORT}`);
+});
